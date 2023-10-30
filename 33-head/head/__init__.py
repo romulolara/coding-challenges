d18b82b2f05d1abd0d9bcfeb87e774f46f002af9
romulo.lara <romulo.lara@dataprev.gov.br>
2023-10-29 22:05:22 -0300

    cchead step3

--- 33-head/head/__init__.py
+++ 33-head/head/__init__.py
@@ -11,6 +11,7 @@ def args_handler():
     parser = argparse.ArgumentParser(description='Displays the first N lines or bytes of a file.')
     parser.add_argument('filename' , nargs="?", help='file to be read.')
     parser.add_argument("-n", required=False, type=int, dest="total_lines", default=10)
+    parser.add_argument("-c", required=False, type=int, dest="total_bytes")
     try:
         namespace = parser.parse_args()
         return namespace
@@ -19,26 +20,42 @@ def args_handler():
         exit(1)
 
 
-def no_file_handler(lines_to_read):
-    for line in range(lines_to_read):
+def no_file_handler(lines_to_read, bytes_to_read):
+    while bytes_to_read:
         text = input()
-        print(text)
-    sys.exit(0)
+        text_read = text[:bytes_to_read]
+        print(text_read)
+        bytes_to_read -= len(text_read)
+
+        if bytes_to_read <= 0:
+            break;
+    else:
+        for line in range(lines_to_read):
+            text = input()
+            print(text)
 
 
-def file_handler(filename, lines_to_read):
-    file_ref = open(filename, "r")
-    lines_read = 0
-    for line in file_ref:
-        if lines_read >= lines_to_read:
-            break
-        print(line.rstrip("\n"))
-        lines_read += 1
-    file_ref.close()
-    sys.exit(0)
+def file_handler(filename, lines_to_read, bytes_to_read):
+    if bytes_to_read:
+        file_ref = open(filename, "rb")
+        text = file_ref.read(bytes_to_read)
+        text = text.decode("utf-8")
+        print(text.rstrip("\n"))
+        file_ref.close()
+    else:
+        file_ref = open(filename, "r")
+        lines_read = 0
+        for line in file_ref:
+            if lines_read >= lines_to_read:
+                break
+            print(line.rstrip("\n"))
+            lines_read += 1
+        file_ref.close()
 
 
 signal.signal(signal.SIGINT, signal_handler)
 
 args = args_handler();
-file_handler(args.filename, args.total_lines) if args.filename else no_file_handler(args.total_lines)
+file_handler(args.filename, args.total_lines, args.total_bytes) if args.filename else no_file_handler(args.total_lines, args.total_bytes)
+
+sys.exit(0)
