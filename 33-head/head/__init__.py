db62c3c48c38f085ae7b7b1368588b8052cf3d92
romulo.lara <romulo.lara@dataprev.gov.br>
2023-10-29 21:10:17 -0300

    cchead step2

--- 33-head/head/__init__.py
+++ 33-head/head/__init__.py
@@ -10,6 +10,7 @@ def signal_handler(signal, frame):
 def args_handler():
     parser = argparse.ArgumentParser(description='Displays the first N lines or bytes of a file.')
     parser.add_argument('filename' , nargs="?", help='file to be read.')
+    parser.add_argument("-n", required=False, type=int, dest="total_lines", default=10)
     try:
         namespace = parser.parse_args()
         return namespace
@@ -18,14 +19,14 @@ def args_handler():
         exit(1)
 
 
-def no_file_handler():
-    for line in range(10):
+def no_file_handler(lines_to_read):
+    for line in range(lines_to_read):
         text = input()
         print(text)
     sys.exit(0)
 
 
-def file_handler(filename, lines_to_read = 10):
+def file_handler(filename, lines_to_read):
     file_ref = open(filename, "r")
     lines_read = 0
     for line in file_ref:
@@ -40,4 +41,4 @@ def file_handler(filename, lines_to_read = 10):
 signal.signal(signal.SIGINT, signal_handler)
 
 args = args_handler();
-file_handler(args.filename) if args.filename else no_file_handler()
+file_handler(args.filename, args.total_lines) if args.filename else no_file_handler(args.total_lines)
